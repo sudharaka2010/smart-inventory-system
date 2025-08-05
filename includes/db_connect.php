68fdb86d553466b05fb4fe6709b96b32957a8af3
Sudharaka Lakshan <sudharaka.lakshan@outlook.com>
2025-08-06 01:41:03 +0530

    Update db_connect.php
    Support Heroku JawsDB and local MySQL

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,11 +1,25 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "rb_stores_db";
+// Check if JAWSDB_URL is available (Heroku environment)
+if (getenv("JAWSDB_URL")) {
+    // Parse JawsDB MySQL URL
+    $url = parse_url(getenv("JAWSDB_URL"));
 
+    $host = $url["host"];
+    $user = $url["user"];
+    $pass = $url["pass"];
+    $dbname = substr($url["path"], 1);
+} else {
+    // Local XAMPP MySQL settings
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $dbname = "rb_stores_db";
+}
+
+// Create connection
 $conn = new mysqli($host, $user, $pass, $dbname);
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
