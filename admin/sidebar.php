b4bac8ab4b579ea13948fb17081f7487ae567c14
Sudharaka Lakshan <sudharaka.lakshan@outlook.com>
2025-08-08 02:28:44 +0530

    Update sidebar.php

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -10,7 +10,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
 
   <!-- Sidebar Stylesheet -->
-  <link rel="stylesheet" href="/rbstorsg/assets/css/sidebar.css" />
+  <link rel="stylesheet" href="/assets/css/sidebar.css" />
 </head>
 <body>
 
